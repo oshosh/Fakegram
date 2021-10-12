a307dc64b600024753d552afc77cbc18cbc7062f
osh <dhtpgus7@gmail.com>
2021-10-12 22:17:59 +0900

    postform 사진만 올릴 수 있도록 파일 확장자 제한 및 용량 제한

--- front/components/postForm/PostForm.js
+++ front/components/postForm/PostForm.js
@@ -1,143 +1,96 @@
 import React, { useCallback, useEffect, useRef, useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
-import { Avatar, Button, Form, Input } from 'antd'
+import { Avatar, Button, Form, Input } from 'antd';
 import styled from 'styled-components';
 import { PictureOutlined, SmileOutlined } from '@ant-design/icons';
-import dynamic from 'next/dynamic'
+import dynamic from 'next/dynamic';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { addPost } from '../../reducers/post'
+import { addPost } from '../../reducers/post';
 
-import Loading from '../common/Loading'
-import Portal from '../common/Portal'
-
-const ContentWrapper = styled.div`
-    margin-top: 20px;
-    border: 1px solid  #d2d2d2;
-    border-radius: 5px;
-    background: #fff;
-    /* width: 614px; */
-
-    & .avatar {
-        display: flex;
-        align-items: center;
-        margin: 1rem;
-
-        & .my-avatar {
-            margin-right: 10px;
-        }
-
-        & span {
-            font-weight: 700;
-        }
-    }
-    & .text-form{
-        & .text-area{
-        
-     }
-    }
-    /* @media screen and (max-width: "1024px") {
-        width: auto;
-    } */
-`
-
-const ContentAddWrapper = styled.div`
-    padding: 16px 0;
-
-    & .content-item{
-        display: flex;
-        align-items: center;
-        justify-content: flex-end;
-        
-        border-radius: 5px;
-        margin: 0 16px ;
-        
-        & span {
-            font-weight: 700;
-        }
-    }
-
-    & .content-confirm-btn {
-        margin: 1rem;
-    }
-`
-const ContentItem = styled.div`
-    display: flex;
-
-    & .picture-content{
-        margin-right: 10px;
-    }
-`
-
-const PickerContainer = styled.div`
-    position: absolute;
-    z-index: 3;
-    top: 200px;
-    right: 100px;
-`
-
+import Loading from '../common/Loading';
+import Portal from '../common/Portal';
 
 function PostForm() {
-    const Picker = dynamic(() => import("emoji-picker-react")) // no ssr
+    const Picker = dynamic(() => import('emoji-picker-react')); // no ssr
     const { register, handleSubmit, watch, setValue, control } = useForm();
-    const { addPostDone, addPostLoading } = useSelector((state) => state.post)
-    const dispatch = useDispatch()
+    const { addPostDone, addPostLoading } = useSelector((state) => state.post);
+    const dispatch = useDispatch();
 
-    const textareaRef = useRef()
-    const imageInput = useRef()
+    const textareaRef = useRef();
+    const imageInput = useRef();
 
-    const [emoji, setEmoji] = useState('')
-    const [open, setOpen] = useState(false)
-    const [textAreaData, setTextAreaData] = useState('')
+    const [emoji, setEmoji] = useState('');
+    const [open, setOpen] = useState(false);
+    const [textAreaData, setTextAreaData] = useState('');
 
     useEffect(() => {
         if (addPostDone) {
-            setTextAreaData('')
-            setValue("text", '')
+            setTextAreaData('');
+            setValue('text', '');
         }
-    }, [addPostDone])
+    }, [addPostDone]);
 
     useEffect(() => {
         if (addPostLoading) {
         }
-    }, [addPostLoading])
+    }, [addPostLoading]);
 
-    const LoadingPortal = () => {
-        return (
-            <>
-                <Portal
-                    elementId={'#loading'}
-                >
-                    <Loading type="spokes" color="blue" message={'데이터 처리중 입니다.'} />
-                </Portal>
-            </>
-        )
-    }
+    const onPickOpenUp = useCallback(() => setOpen(!open), [open]);
 
-    const onPickOpenUp = useCallback(() => {
-        setOpen(!open)
-    }, [open])
+    const onFormSubmit = useCallback(
+        (e) => {
+            if (
+                Object.values(watch()).join().trim() === '' &&
+                textareaRef.current.resizableTextArea.props.value === ''
+            ) {
+                return false;
+            }
+            // 이모지로 ref 통해서 이전 props 값 가져와서 다시 register의 값을 갱신해줘야함..
+            setValue('text', textareaRef.current.resizableTextArea.props.value);
+            dispatch(addPost(textAreaData));
+        },
+        [setValue, watch, textAreaData],
+    );
 
-    const onFormSubmit = useCallback((e) => {
-        if (Object.values(watch()).join().trim() === '' && textareaRef.current.resizableTextArea.props.value === '') {
-            return;
+    const onEmojiClickEvent = useCallback(
+        (e, emojiObject) => {
+            setEmoji(emojiObject.emoji);
+            setOpen(!open);
+            setTextAreaData((prevData) => prevData + emojiObject.emoji);
+        },
+        [emoji, open, textAreaData],
+    );
+
+    const onClickImageUpload = useCallback(
+        (e) => {
+            imageInput.current.click();
+        },
+        [imageInput.current],
+    );
+
+    const onChangeImages = useCallback((e) => {
+        let size = 0;
+        const imageFormData = new FormData();
+        const limitedSize = 10;
+
+        [].forEach.call(e.target.files, (f) => {
+            imageFormData.append('image', f);
+            imageFormData.append('size', f);
+        });
+
+        imageFormData.getAll('size').forEach((item) => {
+            return size += item.size
+        })
+
+        imageFormData.delete('size');
+
+        if (size > (limitedSize * (1024 * 1024))) {
+            return false;
         }
-        setValue("text", textareaRef.current.resizableTextArea.props.value) // 이모지로 ref 통해서 이전 props 값 가져와서 다시 register의 값을 갱신해줘야함..
-        console.log(watch())
 
-        dispatch(addPost(textAreaData))
-
-    }, [setValue, watch, textAreaData])
-
-    const onEmojiClickEvent = useCallback((e, emojiObject) => {
-        setEmoji(emojiObject.emoji)
-        setOpen(!open)
-        setTextAreaData((prevData) => prevData + emojiObject.emoji)
-    }, [emoji, open, textAreaData])
-
-    const onClickImageUpload = useCallback((e) => {
-        imageInput.current.click()
-    }, [imageInput.current])
+        console.log(imageFormData)
+    }, [])
 
     return (
         <>
@@ -170,22 +123,28 @@ function PostForm() {
                                     name="text"
                                     maxLength={140}
                                     className="text-area"
-                                    placeholder={"osh님, 무슨 생각을 하고 계신가요 ?"}
-                                    onChange={e => {
-                                        setTextAreaData(e.target.value)
-                                        setValue("text", e.target.value)
+                                    placeholder={'osh님, 무슨 생각을 하고 계신가요 ?'}
+                                    onChange={(e) => {
+                                        setTextAreaData(e.target.value);
+                                        setValue('text', e.target.value);
                                     }}
                                     value={textAreaData}
-                                >
-                                </Input.TextArea>
+                                ></Input.TextArea>
                             )}
-                            {...register("text")}
+                            {...register('text')}
                         />
                         <ContentAddWrapper className="test">
                             <div className="content-item">
                                 <ContentItem>
                                     <div className="picture-content">
-                                        <input type="file" multiple hidden ref={imageInput} />
+                                        <input
+                                            type="file"
+                                            accept=".png, .jpg, .jpeg"
+                                            multiple
+                                            hidden
+                                            ref={imageInput}
+                                            onChange={onChangeImages}
+                                        />
                                         <Button
                                             icon={<PictureOutlined />}
                                             onClick={onClickImageUpload}
@@ -198,13 +157,11 @@ function PostForm() {
                                             이모지
                                         </Button>
                                     </div>
-                                    {
-                                        open && (
-                                            <PickerContainer>
-                                                <Picker onEmojiClick={onEmojiClickEvent} />
-                                            </PickerContainer>
-                                        )
-                                    }
+                                    {open && (
+                                        <PickerContainer>
+                                            <Picker onEmojiClick={onEmojiClickEvent} />
+                                        </PickerContainer>
+                                    )}
                                 </ContentItem>
                             </div>
                             <div className="content-confirm-btn">
@@ -222,13 +179,87 @@ function PostForm() {
                 </div>
             </ContentWrapper>
 
-            {
-                addPostLoading && LoadingPortal()
-            }
+            {addPostLoading && LoadingPortal()}
         </>
-
     );
 }
 
 export default PostForm;
 
+const ContentWrapper = styled.div`
+  margin-top: 20px;
+  border: 1px solid #d2d2d2;
+  border-radius: 5px;
+  background: #fff;
+  /* width: 614px; */
+
+  & .avatar {
+    display: flex;
+    align-items: center;
+    margin: 1rem;
+
+    & .my-avatar {
+      margin-right: 10px;
+    }
+
+    & span {
+      font-weight: 700;
+    }
+  }
+  & .text-form {
+    & .text-area {
+    }
+  }
+  /* @media screen and (max-width: "1024px") {
+        width: auto;
+    } */
+`;
+
+const ContentAddWrapper = styled.div`
+  padding: 16px 0;
+
+  & .content-item {
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    border-radius: 5px;
+    margin: 0 16px;
+
+    & span {
+      font-weight: 700;
+    }
+  }
+
+  & .content-confirm-btn {
+    margin: 1rem;
+  }
+`;
+const ContentItem = styled.div`
+  display: flex;
+
+  & .picture-content {
+    margin-right: 10px;
+  }
+`;
+
+const PickerContainer = styled.div`
+  position: absolute;
+  z-index: 3;
+  top: 200px;
+  right: 100px;
+`;
+
+const LoadingPortal = () => {
+    return (
+        <>
+            <Portal elementId={'#loading'}>
+                <Loading
+                    type="spokes"
+                    color="blue"
+                    message={'데이터 처리중 입니다.'}
+                />
+            </Portal>
+        </>
+    );
+};
